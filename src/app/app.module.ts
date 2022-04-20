9f009898f0f1ffbd281ff23d29afa9f3564de3fd
SDOS\javier.fernandez <javier.fernandez@s-dos.es>
2022-04-20 10:23:34 +0200

    chore: remove openApi tool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DEFAULT_I18N_LANG, ENV } from '@app/constants/global.constants';
 import { CoreModule } from '@app/core.module';
 import { translateLoaderFactory } from '@app/locale/translate-loader.factory';
-import { Configuration } from '@data/api';
-import { ApiModule } from '@data/api/api.module';
 import { environment } from '@env';
 import { LayoutModule } from '@layout/layout.module';
 import { TranslateCompiler, TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
@@ -30,12 +28,6 @@ if (environment.apiBaseUrl?.length <= 0) {
       enableSourceMaps: true,
       level: environment.logLevel || NgxLoggerLevel.ERROR
     }),
-    ApiModule.forRoot(
-      () =>
-        new Configuration({
-          basePath: environment.apiBaseUrl
-        })
-    ),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
