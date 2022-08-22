4c546184241f872ba1c585d5fb59f11f4269c68a
Diego González <diego.gonzalez@alten.es>
2022-08-22 16:04:57 +0200

    feat: workflow card detail

--- src/app/modules/app-modules/workflow/submodules/workflow-card-details/workflow-card-details.component.ts
+++ src/app/modules/app-modules/workflow/submodules/workflow-card-details/workflow-card-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { ActivatedRoute, Router } from '@angular/router';
 import WorkflowCardDto from '@data/models/workflows/workflow-card-dto';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-workflow-card-details',
@@ -12,8 +14,20 @@ export class WorkflowCardDetailsComponent implements OnInit {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public relativeTo: any = null;
   public card: WorkflowCardDto = null;
+  public tabSelected: 'information' | 'workOrder' | 'messages' | 'actions' = 'information';
+  public showMode: 'all' | 'semi' | 'individual' = 'all';
+  public labels = {
+    information: marker('common.information'),
+    workOrder: marker('common.workOrder'),
+    messages: marker('common.messages'),
+    actions: marker('common.actions')
+  };
 
   constructor(private route: ActivatedRoute, private router: Router, private location: Location) {}
+
+  @HostListener('window:resize', ['$event']) onResize(event: { target: { innerWidth: number } }) {
+    this.setShowMode(event.target.innerWidth);
+  }
 
   ngOnInit(): void {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -24,10 +38,20 @@ export class WorkflowCardDetailsComponent implements OnInit {
     if (state.card) {
       this.card = JSON.parse(state.card);
     }
+    this.setShowMode(window.innerWidth);
+  }
+
+  public setShowMode(width: number) {
+    let showMode: 'all' | 'semi' | 'individual' = 'all';
+    if (width <= 1300 && width > 1050) {
+      showMode = 'semi';
+    } else if (width <= 1050) {
+      showMode = 'individual';
+    }
+    this.showMode = showMode;
   }
 
   public close(): void {
-    console.log(this.route, this.route.parent.snapshot.params);
     if (this.relativeTo) {
       this.router.navigate([{ outlets: { card: null } }], {
         relativeTo: this.relativeTo
@@ -36,5 +60,17 @@ export class WorkflowCardDetailsComponent implements OnInit {
       const currentUrl = window.location.hash.split('#/').join('/').split('/(card:')[0];
       this.router.navigateByUrl(currentUrl);
     }
+  }
+
+  public isTabSelected(tab: 'information' | 'workOrder' | 'messages' | 'actions'): boolean {
+    return this.tabSelected === tab;
+  }
+
+  public changeSelectedTab(tab: 'information' | 'workOrder' | 'messages' | 'actions'): void {
+    this.tabSelected = tab;
+  }
+
+  public getContainerClass(): string {
+    return this.showMode + ' ' + this.tabSelected;
   }
 }
