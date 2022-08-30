d8c4f8f36ac7502e20564653420390bbe3ed488d
Fernando Jesús GONZÁLEZ MARTÍ <fernandoj.gonzalez@sdos.local>
2022-08-30 10:11:56 +0200

    Fix pass pattern

--- src/app/core/constants/patterns.constants.ts
+++ src/app/core/constants/patterns.constants.ts
@@ -1,1 +1,1 @@
-export const passwordPattern = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$¡!%*¿?&._])[A-Za-z\\d@$¡!%*¿?&._]{8,50}$');
+export const passwordPattern = new RegExp('^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()_.]).{8,50}$');
