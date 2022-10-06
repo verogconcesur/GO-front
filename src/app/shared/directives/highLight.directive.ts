a29cd37b7626e80b0261375fb41c3b0d91ae0878
Diego González <diego.gonzalez@alten.es>
2022-10-06 13:13:14 +0200

    feat: highLight.directive

--- src/app/shared/directives/highLight.directive.ts
+++ src/app/shared/directives/highLight.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, Input, SimpleChanges, Renderer2, ElementRef, OnChanges } from '@angular/core';
+import { normalizaStringToLowerCase } from '@shared/utils/string-normalization-lower-case';
 
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
@@ -21,7 +22,22 @@ export class HighlightDirective implements OnChanges {
   }
 
   getFormattedText() {
+    let textNormalized = normalizaStringToLowerCase(this.text);
+    this.searchedWords.map((s) => normalizaStringToLowerCase(s));
     const re = new RegExp(`(${this.searchedWords.join('|')})`, 'g');
-    return this.text.replace(re, `<span class="${this.classToApply}">$1</span>`);
+    textNormalized = textNormalized.replace(re, `<mark class="${this.classToApply}">$1<emark>`);
+    return this.replaceMarksInOriginalText(textNormalized, this.text);
+  }
+
+  replaceMarksInOriginalText(textNormalized: string, original: string): string {
+    const initialMark = `<mark class="${this.classToApply}">`;
+    const endMark = '<emark>';
+    let pos = textNormalized.indexOf(initialMark);
+    if (pos >= 0) {
+      original = `${original.substring(0, pos)}${initialMark}${original.substring(pos)}`;
+      pos = textNormalized.indexOf('<emark>');
+      original = `${original.substring(0, pos)}</mark>${original.substring(pos)}`;
+    }
+    return original;
   }
 }
