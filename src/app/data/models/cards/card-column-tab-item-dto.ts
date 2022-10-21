1e9d0c6c4e8301be1f4bfb584f6a6e8e81606b3c
Fernando Jesús GONZÁLEZ MARTÍ <fernandoj.gonzalez@sdos.local>
2022-10-21 09:22:36 +0200

    HU131 - CONFIG PESTAÑA PERSONALIZABLE LIBRE

--- src/app/data/models/cards/card-column-tab-item-dto.ts
+++ src/app/data/models/cards/card-column-tab-item-dto.ts
@@ -5,7 +5,96 @@ export default interface CardColumnTabItemDTO {
   orderNumber: number;
   name: string;
   description: string;
-  tabItemConfigVariable?: { id: number; variable: { id: number; attributeName: string; value: string }; visible: boolean };
-  tabItemConfigAction?: { id: number; tabItemId: number; actionType: string; visible: boolean };
-  tabItemConfigLink?: { id: number; tabItemId: number; link: string; color: string };
+  tabItemConfigAction?: TabItemConfigActionDTO;
+  tabItemConfigInput?: TabItemConfigInputDTO;
+  tabItemConfigLink?: TabItemConfigLinkDTO;
+  tabItemConfigList?: TabItemConfigListDTO;
+  tabItemConfigOption?: TabItemConfigOptionDTO;
+  tabItemConfigTable?: TabItemConfigTableDTO;
+  tabItemConfigText?: TabItemConfigTextDTO;
+  tabItemConfigTitle?: TabItemConfigTitleDTO;
+  tabItemConfigVariable?: TabItemConfigVariableDTO;
+}
+export interface TabItemConfigActionDTO {
+  id?: number;
+  tabItemId?: number;
+  actionType: 'SIGN_DOC' | 'MESSAGE_CLIENT' | 'ATTACH_DOC';
+  visible: boolean;
+}
+export interface TabItemConfigInputDTO {
+  id?: number;
+  tabItemId?: number;
+  description: string;
+  dataType: 'STRING' | 'NUMBER' | 'TEMPORAL';
+  dateApplyColor?: string;
+  dateColor?: boolean;
+  dateLimit?: boolean;
+  dateType?: 'DATETIME' | 'DATE' | 'TIME';
+  mandatory?: boolean;
+  numDecimals?: number;
+}
+export interface TabItemConfigCommonDTO {
+  id?: number;
+  tabItemId?: number;
+}
+export interface TabItemConfigLinkDTO extends TabItemConfigCommonDTO {
+  link: string;
+  color: string;
+}
+export interface TabItemConfigListDTO extends TabItemConfigCommonDTO {
+  description?: string;
+  code: string;
+  listItems: TabItemConfigListItemDTO[];
+  mandatory?: boolean;
+  parentCode: string;
+  selectionType: 'SIMPLE' | 'MULTIPLE';
+}
+export interface TabItemConfigListItemDTO extends TabItemConfigCommonDTO {
+  tabItemConfigListId?: number;
+  value: string;
+  code: string;
+  parentCode: string;
+}
+export interface TabItemConfigOptionDTO extends TabItemConfigCommonDTO {
+  description?: string;
+  applyColor: string;
+  color: string;
+  overridePriority: boolean;
+}
+export interface TabItemConfigTableDTO extends TabItemConfigCommonDTO {
+  description?: string;
+  readOption: 'READONLY' | 'READWRITE';
+  tabItemConfigTableCols: TabItemConfigTableColDTO[];
+}
+export interface TabItemConfigTableColDTO extends TabItemConfigCommonDTO {
+  description?: string;
+  dataType: 'STRING' | 'NUMBER' | 'BOOLEAN';
+  header: string;
+  mandatory?: boolean;
+  orderNumber: number;
+  tabItemConfigTableId?: number;
+  tabItemTableColValues: TabItemTableColValueDTO[];
+}
+export interface TabItemTableColValueDTO extends TabItemConfigCommonDTO {
+  rowCode?: number;
+  tabItemConfigTableColId?: number;
+  valueBoolean?: boolean;
+  valueNumber?: number;
+  valueString?: string;
+}
+export interface TabItemConfigTextDTO extends TabItemConfigCommonDTO {
+  description?: string;
+  value?: string;
+}
+export interface TabItemConfigTitleDTO extends TabItemConfigCommonDTO {
+  description?: string;
+  value?: string;
+}
+export interface TabItemConfigVariableDTO extends TabItemConfigCommonDTO {
+  variable: {
+    id: number;
+    attributeName: string;
+    value: string;
+  };
+  visible: boolean;
 }
