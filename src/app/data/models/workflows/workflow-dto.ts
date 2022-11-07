6328fdb7ad349c7756a418eba8e2f6403ccb52bd
PROD\fernandoj.gonzalez <fernandoj.gonzalez@alten.es>
2022-11-07 12:13:36 +0100

    feat: create-edit workflow implementation

--- src/app/data/models/workflows/workflow-dto.ts
+++ src/app/data/models/workflows/workflow-dto.ts
@@ -2,7 +2,10 @@ import BrandDTO from '../organization/brand-dto';
 import DepartmentDTO from '../organization/department-dto';
 import FacilityDTO from '../organization/facility-dto';
 import SpecialtyDTO from '../organization/specialty-dto';
-
+export enum WorkFlowStatusEnum {
+  draft = 'DRAFT',
+  published = 'PUBLISHED'
+}
 export default interface WorkflowDTO {
   id: number;
   name: string;
