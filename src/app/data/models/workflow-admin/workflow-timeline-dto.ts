05c2b708d2b7dc9495e5e27cb77cec5e917ef2f2
PROD\fernandoj.gonzalez <fernandoj.gonzalez@alten.es>
2022-11-23 14:45:47 +0100

    feat: wokrflow timeline implementation

--- src/app/data/models/workflow-admin/workflow-timeline-dto.ts
+++ src/app/data/models/workflow-admin/workflow-timeline-dto.ts
@@ -2,12 +2,11 @@ import TemplatesTimelineDTO, { TemplatesTimelineItemsDTO } from '../templates/te
 import WorkflowSubstateDTO from '../workflows/workflow-substate-dto';
 
 export interface WorkflowTimelineDTO {
-  id?: number;
   templateTimelineDTO?: TemplatesTimelineDTO;
   workflowSubstateTimelineItems?: WorkflowSubstateTimelineItemDTO[];
 }
 
 export interface WorkflowSubstateTimelineItemDTO {
   templateTimelineItem?: TemplatesTimelineItemsDTO;
-  workflowSubstate?: WorkflowSubstateDTO[];
+  workflowSubstate?: WorkflowSubstateDTO;
 }
--- src/app/modules/app-modules/administration/workflows/components/create-edit-steps/workflow-timeline/workflow-timeline.component.ts
+++ src/app/modules/app-modules/administration/workflows/components/create-edit-steps/workflow-timeline/workflow-timeline.component.ts
@@ -1,11 +1,15 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { identifierName } from '@angular/compiler';
 import { Component, Input } from '@angular/core';
 import { FormArray, FormControl, FormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { marker } from '@biesbjerg/ngx-translate-extract-marker';
 import TemplatesCommonDTO from '@data/models/templates/templates-common-dto';
-import { TemplatesTimelineItemsDTO } from '@data/models/templates/templates-timeline-dto';
+import TemplatesTimelineDTO, { TemplatesTimelineItemsDTO } from '@data/models/templates/templates-timeline-dto';
 import { WorkflowSubstateTimelineItemDTO, WorkflowTimelineDTO } from '@data/models/workflow-admin/workflow-timeline-dto';
 import WorkflowStateDTO from '@data/models/workflows/workflow-state-dto';
 import WorkflowSubstateDTO from '@data/models/workflows/workflow-substate-dto';
+import { TemplatesTimelineService } from '@data/services/templates-timeline.service';
+import { WorkflowAdministrationStatesSubstatesService } from '@data/services/workflow-administration-states-substates.service';
 import { WorkflowAdministrationService } from '@data/services/workflow-administration.service';
 import { TranslateService } from '@ngx-translate/core';
 import { ConfirmDialogService } from '@shared/services/confirm-dialog.service';
@@ -26,10 +30,13 @@ export class WorkflowTimelineComponent extends WorkflowStepAbstractClass {
   @Input() stepIndex: number;
   public labels = {
     title: marker('workflows.selectTimeline'),
-    label: marker('workflows.timeline')
+    timelineStates: marker('workflows.timelineStates'),
+    label: marker('workflows.timelineLabel'),
+    required: marker('errors.required')
   };
   public templateForm: FormControl;
   public substateList: WorkflowSubstateDTO[] = [];
+  public substateDragging: WorkflowSubstateDTO;
   constructor(
     private fb: UntypedFormBuilder,
     public workflowsCreateEditAuxService: WorkflowsCreateEditAuxService,
@@ -37,19 +44,28 @@ export class WorkflowTimelineComponent extends WorkflowStepAbstractClass {
     public translateService: TranslateService,
     private spinnerService: ProgressSpinnerDialogService,
     public workflowService: WorkflowAdministrationService,
+    public workflowStateService: WorkflowAdministrationStatesSubstatesService,
+    public timelineService: TemplatesTimelineService,
     private logger: NGXLogger
   ) {
     super(workflowsCreateEditAuxService, confirmationDialog, translateService);
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public initForm(data: any): void {
-    this.originalData.stateList.forEach((state: WorkflowStateDTO) => {
-      this.substateList = [...this.substateList, ...state.workflowSubstates];
-    });
+    if (data && data.stateList) {
+      data.stateList.forEach((state: WorkflowStateDTO) => {
+        state.workflowSubstates.forEach((substate: WorkflowSubstateDTO) => {
+          substate.workflowState = { ...state, workflowSubstates: [] };
+          this.substateList.push(substate);
+        });
+      });
+    }
     this.templateForm = new FormControl(null, [Validators.required]);
-    if (data.workflowTimeline) {
+    if (data && data.workflowTimeline && data.workflowTimeline.templateTimelineDTO) {
       this.templateForm.setValue(
-        this.originalData.find((template: TemplatesCommonDTO) => template.id === data.workflowTimeline.templateTimelineDTO.id)
+        data.timelineTemplates.find(
+          (template: TemplatesCommonDTO) => template.id === data.workflowTimeline.templateTimelineDTO.template.id
+        )
       );
     }
     this.form = this.fb.group({
@@ -62,26 +78,94 @@ export class WorkflowTimelineComponent extends WorkflowStepAbstractClass {
       });
     }
   }
-  public generateTimelineItem(timelineItem: WorkflowSubstateTimelineItemDTO): FormGroup {
+  public generateTimelineItem(workflowTimelineItem: WorkflowSubstateTimelineItemDTO): FormGroup {
+    workflowTimelineItem.workflowSubstate = this.substateList.find(
+      (substate: WorkflowSubstateDTO) => substate.id === workflowTimelineItem.workflowSubstate.id
+    );
     return this.fb.group({
-      templateTimelineItem: [timelineItem.templateTimelineItem, [Validators.required]],
-      workflowSubstateTimelineItems: [timelineItem.workflowSubstate ? timelineItem.workflowSubstate : []]
+      templateTimelineItem: [workflowTimelineItem.templateTimelineItem],
+      workflowSubstate: [workflowTimelineItem.workflowSubstate, [Validators.required]]
     });
+  }
+  public generateTimelineItemByTemplate(substate: WorkflowSubstateDTO): FormGroup {
+    return this.fb.group({
+      templateTimelineItem: [],
+      workflowSubstate: [substate]
+    });
+  }
+  public setSubstateDragging(substate: WorkflowSubstateDTO): void {
+    this.substateDragging = substate;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public dropSubstate(timeline: TemplatesTimelineItemsDTO): void {
+    const timelineLineIndex = this.form
+      .get('workflowSubstateTimelineItems')
+      .getRawValue()
+      .findIndex((timeLine: WorkflowSubstateTimelineItemDTO) => timeLine.workflowSubstate.id === this.substateDragging.id);
+    this.form
+      .get('workflowSubstateTimelineItems')
+      .get(timelineLineIndex.toString())
+      .get('templateTimelineItem')
+      .setValue(timeline);
+    this.form.markAsTouched();
+    this.form.markAsDirty();
+  }
+  public getSubstatesByTimeline(timelineItem?: TemplatesTimelineItemsDTO): WorkflowSubstateDTO[] {
+    let timelineLineItems = this.form.get('workflowSubstateTimelineItems').getRawValue() as WorkflowSubstateTimelineItemDTO[];
+    if (timelineItem) {
+      timelineLineItems = timelineLineItems.filter(
+        (timeLine: WorkflowSubstateTimelineItemDTO) =>
+          timeLine.templateTimelineItem && timeLine.templateTimelineItem.id === timelineItem.id
+      );
+      return timelineLineItems.map((timeLine: WorkflowSubstateTimelineItemDTO) => timeLine.workflowSubstate);
+    } else {
+      timelineLineItems = timelineLineItems.filter((timeLine: WorkflowSubstateTimelineItemDTO) => !timeLine.templateTimelineItem);
+      return timelineLineItems.map((timeLine: WorkflowSubstateTimelineItemDTO) => timeLine.workflowSubstate);
+    }
+  }
+  public removeTimeLine(substate: WorkflowSubstateDTO): void {
+    const index = this.form
+      .get('workflowSubstateTimelineItems')
+      .getRawValue()
+      .findIndex((timeLine: WorkflowSubstateTimelineItemDTO) => timeLine.workflowSubstate.id === substate.id);
+    this.form.get('workflowSubstateTimelineItems').get(index.toString()).get('templateTimelineItem').setValue(null);
+    this.form.markAsTouched();
+    this.form.markAsDirty();
+  }
+  public changeTemplate() {
+    if (this.templateForm.value) {
+      const spinner = this.spinnerService.show();
+      this.timelineService
+        .findById(this.templateForm.value.id)
+        .pipe(take(1))
+        .subscribe((res: TemplatesTimelineDTO) => {
+          this.form.get('templateTimelineDTO').setValue(res);
+          const formArray = this.form.get('workflowSubstateTimelineItems') as FormArray;
+          while (formArray.length > 0) {
+            formArray.removeAt(0);
+          }
+          this.substateList.forEach((substate: WorkflowSubstateDTO) => {
+            (this.form.get('workflowSubstateTimelineItems') as FormArray).push(this.generateTimelineItemByTemplate(substate));
+          });
+          this.form.markAsTouched();
+          this.form.markAsDirty();
+          this.spinnerService.hide(spinner);
+        });
+    }
   }
   public async getWorkflowStepData(): Promise<boolean> {
     const spinner = this.spinnerService.show();
     return new Promise((resolve, reject) => {
-      const states = this.workflowsCreateEditAuxService.getFormGroupByStep(5).getRawValue() as WorkflowStateDTO[];
       const resquests = [
         this.workflowService.getWorkflowTimeline(this.workflowId).pipe(take(1)),
         this.workflowService.getTemplates(this.workflowId, 'TIMELINE').pipe(take(1)),
-        // this.workflowService.getWorkflowStates(this.workflowId).pipe(take(1))
+        this.workflowStateService.getWorkflowStatesAndSubstates(this.workflowId).pipe(take(1))
       ];
       forkJoin(resquests).subscribe(
-        (responses: [WorkflowTimelineDTO, TemplatesCommonDTO[], WorkflowStateDTO]) => {
+        (responses: [WorkflowTimelineDTO, TemplatesCommonDTO[], WorkflowStateDTO[]]) => {
           this.originalData = {
             workflowTimeline: responses[0],
-            timelineTemplates: responses[1],
+            timelineTemplates: responses[1] ? responses[1] : [],
             stateList: responses[2]
           };
           this.spinnerService.hide(spinner);
@@ -98,9 +182,13 @@ export class WorkflowTimelineComponent extends WorkflowStepAbstractClass {
   public async saveStep(): Promise<boolean> {
     const spinner = this.spinnerService.show();
     return new Promise((resolve, reject) => {
-      const timelineBody = this.form.getRawValue();
+      const body = this.form.getRawValue() as WorkflowTimelineDTO;
+      body.workflowSubstateTimelineItems = body.workflowSubstateTimelineItems.filter(
+        (timelineItem) => timelineItem.templateTimelineItem
+      );
+      console.log(JSON.stringify(body));
       this.workflowService
-        .postWorkflowTimeline(this.workflowId, timelineBody)
+        .postWorkflowTimeline(this.workflowId, body)
         .pipe(
           take(1),
           finalize(() => {
@@ -109,7 +197,6 @@ export class WorkflowTimelineComponent extends WorkflowStepAbstractClass {
         )
         .subscribe({
           next: (response) => {
-            this.initForm(timelineBody);
             resolve(true);
           },
           error: (err) => {
