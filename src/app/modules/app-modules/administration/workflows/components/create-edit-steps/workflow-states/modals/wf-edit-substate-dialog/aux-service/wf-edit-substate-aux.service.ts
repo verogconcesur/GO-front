aa7c952ccac3aa2630f5c9cdf224c46ab55573d5
Diego GONZALEZ DEL CAMPO <diego.gonzalez@sdos.local>
2022-11-29 11:19:17 +0100

    feat: wf substates events tab

--- src/app/modules/app-modules/administration/workflows/components/create-edit-steps/workflow-states/modals/wf-edit-substate-dialog/aux-service/wf-edit-substate-aux.service.ts
+++ src/app/modules/app-modules/administration/workflows/components/create-edit-steps/workflow-states/modals/wf-edit-substate-dialog/aux-service/wf-edit-substate-aux.service.ts
@@ -1,7 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Injectable } from '@angular/core';
 import { FormBuilder, UntypedFormGroup } from '@angular/forms';
-import { of, Subject } from 'rxjs';
+import { marker } from '@biesbjerg/ngx-translate-extract-marker';
+import TreeNode from '@data/interfaces/tree-node';
+import WorkflowRoleDTO from '@data/models/workflow-admin/workflow-role-dto';
+import WorkflowMoveDTO from '@data/models/workflows/workflow-move-dto';
+import WorkflowStateDTO from '@data/models/workflows/workflow-state-dto';
+import WorkflowSubstateDTO from '@data/models/workflows/workflow-substate-dto';
+import WorkflowSubstateEventDTO from '@data/models/workflows/workflow-substate-event-dto';
+import { WorkflowAdministrationStatesSubstatesService } from '@data/services/workflow-administration-states-substates.service';
+import { WorkflowAdministrationService } from '@data/services/workflow-administration.service';
+import { TranslateService } from '@ngx-translate/core';
+import { GlobalMessageService } from '@shared/services/global-message.service';
+import { ProgressSpinnerDialogService } from '@shared/services/progress-spinner-dialog.service';
+import { NGXLogger } from 'ngx-logger';
+import { forkJoin, of, Subject } from 'rxjs';
+import { finalize, take } from 'rxjs/operators';
 
 export enum TAB_IDS {
   GENERAL = 'GENERAL',
@@ -18,10 +32,20 @@ export class WEditSubstateFormAuxService {
   public nextStep$ = new Subject();
   public resetForm$ = new Subject();
   public saveActionClicked = false;
+  public allStatesAndSubstates: TreeNode[];
+  public workflowRoles: WorkflowRoleDTO[];
   private formsOriginalData: any = {};
   private form: UntypedFormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private workflowService: WorkflowAdministrationService,
+    private substatesService: WorkflowAdministrationStatesSubstatesService,
+    public spinnerService: ProgressSpinnerDialogService,
+    private logger: NGXLogger,
+    private translateService: TranslateService,
+    private globalMessageService: GlobalMessageService
+  ) {
     this.initForm();
   }
 
@@ -68,6 +92,59 @@ export class WEditSubstateFormAuxService {
     this.resetForm$.next(true);
   }
 
+  public fetchAllStatesSubstatesAndRoles(
+    workflowId: number,
+    state: WorkflowStateDTO,
+    substate: WorkflowSubstateDTO
+  ): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      const resquests = [
+        this.workflowService.getWorkflowUserRoles(workflowId).pipe(take(1)),
+        this.substatesService.getAllStatesAndSubstates().pipe(take(1))
+      ];
+      forkJoin(resquests).subscribe(
+        (responses: [WorkflowRoleDTO[], WorkflowStateDTO[]]) => {
+          this.workflowRoles = responses[0];
+          this.allStatesAndSubstates = [];
+          if (responses[1]?.length) {
+            this.allStatesAndSubstates = this.createTree(responses[1], workflowId, state, substate);
+          }
+          resolve(true);
+        },
+        (error) => {
+          this.logger.error(error);
+          this.globalMessageService.showError({
+            message: error.message,
+            actionText: this.translateService.instant(marker('common.close'))
+          });
+          resolve(false);
+        }
+      );
+    });
+  }
+
+  public newEmptyMove(): WorkflowSubstateEventDTO {
+    return {
+      id: null,
+      requiredFields: false,
+      requiredFieldsList: [],
+      requiredHistoryComment: false,
+      requiredMyself: false,
+      requiredSize: false,
+      requiredUser: false,
+      sendMail: false,
+      sendMailAuto: false,
+      sendMailReceiverRole: null,
+      sendMailReceiverType: null,
+      sendMailTemplate: null,
+      signDocument: false,
+      signDocumentTemplate: null,
+      movementExtraAuto: false,
+      movementExtraConfirm: false,
+      requiredMovementExtra: false
+    };
+  }
+
   private initForm(): void {
     this.form = this.fb.group({
       GENERAL: this.fb.group({}),
@@ -75,5 +152,81 @@ export class WEditSubstateFormAuxService {
       MOVEMENTS: this.fb.group({}),
       EVENTS: this.fb.group({})
     });
+  }
+
+  private createTree(
+    data: WorkflowStateDTO[],
+    workflowId: number,
+    thisState: WorkflowStateDTO,
+    thisSubstate: WorkflowSubstateDTO
+  ): TreeNode[] {
+    const treeNode: TreeNode[] = [];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const otherWorkflows: any = {};
+    const workflowsIdsFound: number[] = [];
+    data.forEach((state: WorkflowStateDTO) => {
+      if (state.id === thisState.id && thisState.workflowSubstates.length === 1) {
+        return;
+      }
+      if (workflowsIdsFound.indexOf(state.workflow.id) === -1) {
+        //Es un workflow nuevo
+        workflowsIdsFound.push(state.workflow.id);
+        const dataToPush = {
+          ...state.workflow,
+          name: `${this.translateService.instant('cards.workflows')}: ${state.workflow.name}`,
+          children: [
+            {
+              ...state,
+              name: `${this.translateService.instant('common.state')}: ${state.name}`,
+              children: [
+                ...state.workflowSubstates
+                  .filter((wfSubs: WorkflowSubstateDTO) => wfSubs.id !== thisSubstate.id)
+                  .map((wfSubs: WorkflowSubstateDTO) => ({
+                    ...wfSubs,
+                    workflowState: { ...state, children: [], workflowSubstates: [] }
+                  }))
+              ]
+            }
+          ]
+        };
+        if (state.workflow.id === workflowId) {
+          //Es el workflow que estamos editando
+          treeNode.push(dataToPush);
+        } else {
+          //Es otro workflow
+          otherWorkflows[state.workflow.id] = dataToPush;
+        }
+      } else {
+        //Ya tenemos este workflow
+        const dataToPush = {
+          ...state,
+          name: `${this.translateService.instant('common.state')}: ${state.name}`,
+          children: [
+            ...state.workflowSubstates
+              .filter((wfSubs: WorkflowSubstateDTO) => wfSubs.id !== thisSubstate.id)
+              .map((wfSubs: WorkflowSubstateDTO) => ({
+                ...wfSubs,
+                workflowState: { ...state, children: [], workflowSubstates: [] }
+              }))
+          ]
+        };
+        if (state.workflow.id === workflowId) {
+          //Es el workflow que estamos editando
+          treeNode[0].children.push(dataToPush);
+        } else {
+          //Es otro workflow
+          otherWorkflows[state.workflow.id].children.push(dataToPush);
+        }
+      }
+    });
+    const otherWorkflowsNode: TreeNode = {
+      name: this.translateService.instant(marker('cards.otherWorkflows')),
+      children: []
+    };
+    Object.keys(otherWorkflows).forEach((k) => {
+      otherWorkflowsNode.children.push(otherWorkflows[k]);
+    });
+    treeNode.push(otherWorkflowsNode);
+    return treeNode;
   }
 }
