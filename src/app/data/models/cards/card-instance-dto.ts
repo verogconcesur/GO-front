47de7dcabc3dd39defab944a4796e5afe0854c61
Diego González <diego.gonzalez@alten.es>
2022-12-07 13:54:31 +0100

    fix: errors
    Permisos en tab acciones
    Filtro usuario vista tablero
    Mostrar usuario cabecera tarjeta

--- src/app/data/models/cards/card-instance-dto.ts
+++ src/app/data/models/cards/card-instance-dto.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import UserDTO from '../user-permissions/user-dto';
 import WorkflowCardInstanceDTO from '../workflows/workflow-card-instance-dto';
 import CardDTO from './card-dto';
 
@@ -8,7 +9,7 @@ export default interface CardInstanceDTO {
   follower: boolean;
   reference: string;
   repairOrderId: string;
-  users: { cardInstanceWorkflow: any; dateAssignment: number; id: number; userId: number }[];
+  users: { cardInstanceWorkflow: any; dateAssignment: number; id: number; userId: number; user?: UserDTO }[];
   workflowId: number;
   workflowName: string;
   workflowStateId: number;
