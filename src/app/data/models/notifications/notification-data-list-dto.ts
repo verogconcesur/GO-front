e240603cd4a94c30973b70faa0914aa407b5658c
Diego González <diego.gonzalez@alten.es>
2023-03-06 11:16:03 +0100

    feat: show date in notifications and mentions
    fix: show error in workflow delete action

--- src/app/data/models/notifications/notification-data-list-dto.ts
+++ src/app/data/models/notifications/notification-data-list-dto.ts
@@ -1,7 +1,7 @@
 import WorkflowCardDTO from '../workflows/workflow-card-dto';
 
 export default interface NotificationDataListDTO {
-  dateNotification: string;
+  dateNotification: Date;
   id: number;
   notification: string;
   notificationType: ('CHANGE_STATE' | 'EDIT_INFO' | 'NEW_CARD' | 'END_WORK' | 'ADD_COMMENT' | 'ADD_DOC' | 'ADD_MESSAGE_CLIENT')[];
--- src/app/data/services/workflows.service.ts
+++ src/app/data/services/workflows.service.ts
@@ -240,11 +240,15 @@ export class WorkflowsService {
   }
 
   public deleteWorkflow(id: number): Observable<boolean> {
-    return this.http.delete<boolean>(`${this.env.apiBaseUrl}${this.GET_WORKFLOWS_PATH}/${id}`);
+    return this.http
+      .delete<boolean>(`${this.env.apiBaseUrl}${this.GET_WORKFLOWS_PATH}/${id}`)
+      .pipe(catchError((error) => throwError(error.error as ConcenetError)));
   }
 
   public duplicateWorkflow(id: number): Observable<boolean> {
-    return this.http.get<boolean>(`${this.env.apiBaseUrl}${this.GET_WORKFLOWS_PATH}${this.DUPLICATE_WORKFLOWS_PATH}/${id}`);
+    return this.http
+      .get<boolean>(`${this.env.apiBaseUrl}${this.GET_WORKFLOWS_PATH}${this.DUPLICATE_WORKFLOWS_PATH}/${id}`)
+      .pipe(catchError((error) => throwError(error.error as ConcenetError)));
   }
 
   public prepareMovement(card: WorkflowCardDTO, targetId: number): Observable<WorkflowSubstateEventDTO[]> {
