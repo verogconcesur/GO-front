1885df2235571b479fc6d63b70df8888aca661db
Fernando Jesús GONZÁLEZ MARTÍ <fernandoj.gonzalez@sdos.local>
2023-03-09 12:37:02 +0100

    Manual repair order implementation

--- src/app/modules/feature-modules/entities-searcher-dialog/entities-searcher-dialog.module.ts
+++ src/app/modules/feature-modules/entities-searcher-dialog/entities-searcher-dialog.module.ts
@@ -6,6 +6,8 @@ import { ModalCustomerModule } from '../modal-customer/modal-customer.module';
 import { ModalVehicleModule } from '../modal-vehicle/modal-vehicle.module';
 import { ModalCustomerExternalApiModule } from '../modal-customer-external-api/modal-customer-external-api.module';
 import { ModalVehicleExternalApiModule } from '../modal-vehicle-external-api/modal-vehicle-external-api.module';
+import { ModalRepairOrderModule } from '../modal-repair-order/modal-repair-order.module';
+import { ModalRepairOrderExternalApiModule } from '../modal-repair-order-external-api/modal-repair-order-external-api.module';
 
 @NgModule({
   declarations: [EntitiesSearcherDialogComponent],
@@ -13,9 +15,11 @@ import { ModalVehicleExternalApiModule } from '../modal-vehicle-external-api/mod
     CommonModule,
     SharedModule,
     ModalCustomerModule,
+    ModalRepairOrderModule,
     ModalVehicleModule,
     ModalCustomerExternalApiModule,
-    ModalVehicleExternalApiModule
+    ModalVehicleExternalApiModule,
+    ModalRepairOrderExternalApiModule
   ],
   exports: []
 })
