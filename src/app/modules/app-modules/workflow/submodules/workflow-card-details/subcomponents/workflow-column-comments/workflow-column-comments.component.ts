f34f3a5a810ef79e38ad3773fa54da2f621284ae
Diego González <diego.gonzalez@alten.es>
2023-03-15 16:51:11 +0100

    feat: wss connection

--- src/app/modules/app-modules/workflow/submodules/workflow-card-details/subcomponents/workflow-column-comments/workflow-column-comments.component.ts
+++ src/app/modules/app-modules/workflow/submodules/workflow-card-details/subcomponents/workflow-column-comments/workflow-column-comments.component.ts
@@ -17,8 +17,7 @@ import { TextEditorWrapperComponent } from '@modules/feature-modules/text-editor
 import { NotificationSoundService } from '@shared/services/notification-sounds.service';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { RxStompService } from '@app/services/rx-stomp.service';
-import { Message } from '@stomp/stompjs';
-// import { SocketService } from '@app/services/socket.service';
+import { IMessage } from '@stomp/stompjs';
 
 @UntilDestroy()
 @Component({
@@ -59,8 +58,7 @@ export class WorkflowColumnCommentsComponent implements OnInit, OnDestroy {
     private spinnerService: ProgressSpinnerDialogService,
     private notificationSoundService: NotificationSoundService,
     private rxStompService: RxStompService
-  ) // private socketService: SocketService
-  {}
+  ) {}
 
   ngOnInit(): void {
     this.idCard = parseInt(this.route.snapshot.params.idCard, 10);
@@ -68,26 +66,16 @@ export class WorkflowColumnCommentsComponent implements OnInit, OnDestroy {
     this.interval = setInterval(() => {
       // this.getData(false, false, true);
     }, this.timeBeforeMarkAsRead);
-    // setTimeout(() => {
     this.rxStompService
       .watch('/topic/newcomment/workflow-2/facility-1')
       .pipe(untilDestroyed(this))
-      .subscribe((data: Message) => {
-        console.log(data);
+      .subscribe((data: IMessage) => {
+        console.log(data.body);
       });
-    // this.socketService.initializeWebSocketConnection();
-    // setTimeout(() => {
-    // this.socketService
-    //   .onEvent('/topic/newcomment/workflow-2/facility-1')
-    //   .pipe(untilDestroyed(this))
-    //   .subscribe((d) => console.log(d));
-    // }, 5000);
-    // }, 10000);
   }
 
   ngOnDestroy(): void {
     clearInterval(this.interval);
-    // this.socketService.disconect();
     this.interval = null;
   }
 
