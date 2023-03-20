0a67b4ce814404a62cbd77826ce27950f6b3ab0f
Diego González <diego.gonzalez@alten.es>
2023-03-20 09:29:10 +0100

    fix: avoid edit attachcment name with an used name

--- src/app/shared/validators/filename.validator.ts
+++ src/app/shared/validators/filename.validator.ts
@@ -1,16 +1,19 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export default class FilenameValidator {
-  static validate(controlName: string): ValidatorFn {
+  static validate(namesArrayToAvoid?: string[]): ValidatorFn {
     return (control: AbstractControl) => {
       if (control?.value) {
         const name = control.value;
+        namesArrayToAvoid = namesArrayToAvoid ? namesArrayToAvoid.map((n: string) => n.toLowerCase()) : null;
         if (/:|\/|\?|\\|\*|\"|<|>|\||\[|\]|\{|\}|%/g.test(name)) {
           return { filename: true };
         } else if (name.split('.').length !== 2) {
           return { extension: true };
         } else if (name.split('.')[0].length === 0 || name.split('.')[1].length === 0) {
           return { extension: true };
+        } else if (namesArrayToAvoid?.length && namesArrayToAvoid.indexOf(name.toLowerCase()) >= 0) {
+          return { fileNameUsed: true };
         }
         return null;
       } else {
