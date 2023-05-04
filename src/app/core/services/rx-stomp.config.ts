7af651a65a37d13217a9f80ed0dfb298836626a3
Diego González <diego.gonzalez@alten.es>
2023-05-04 13:04:40 +0200

    fix: increase reconection time for websockets

--- src/app/core/services/rx-stomp.config.ts
+++ src/app/core/services/rx-stomp.config.ts
@@ -20,7 +20,7 @@ export const rxStompConfig: RxStompConfig = {
   // Wait in milliseconds before attempting auto reconnect
   // Set to 0 to disable
   // Typical value 500 (500 milli seconds)
-  reconnectDelay: 2000,
+  reconnectDelay: 30000,
 
   // Will log diagnostics on console
   // It can be quite verbose, not recommended in production
