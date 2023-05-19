dfbf6bf7ee5fc9e1d29cb9c25bb747144924f600
Fernando Jesús GONZÁLEZ MARTÍ <fernandoj.gonzalez@sdos.local>
2023-05-19 12:41:38 +0200

    Merge link component

--- src/app/data/services/landing.service.ts
+++ src/app/data/services/landing.service.ts
@@ -4,6 +4,7 @@ import { ENV } from '@app/constants/global.constants';
 import { Env } from '@app/types/env';
 import { ConcenetError } from '@app/types/error';
 import LandingConfigDTO from '@data/models/landing/landing-config-dto';
+import LandingLinkDTO from '@data/models/landing/landing-link-dto';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -15,6 +16,8 @@ export class LandingService {
   private readonly CONFIG_PATH = '/config';
   private readonly GET_CONFIG_PATH = '/getEditConfig';
   private readonly SAVE_CONFIG_PATH = '/saveConfig';
+  private readonly LINKS_PATH = '/listLinks';
+  private readonly LINK_DETAIL_PATH = '/getLink/';
 
   constructor(@Inject(ENV) private env: Env, private http: HttpClient) {}
 
@@ -30,6 +33,16 @@ export class LandingService {
         `${this.env.apiBaseUrl}${this.LANDING_PATH}${this.CONFIG_PATH}${this.SAVE_CONFIG_PATH}`,
         landingConfig
       )
+      .pipe(catchError((error) => throwError(error.error as ConcenetError)));
+  }
+  public getLinks(): Observable<LandingLinkDTO[]> {
+    return this.http
+      .get<LandingLinkDTO[]>(`${this.env.apiBaseUrl}${this.LANDING_PATH}${this.CONFIG_PATH}${this.LINKS_PATH}`)
+      .pipe(catchError((error) => throwError(error.error as ConcenetError)));
+  }
+  public getLinkDetail(linkId: number): Observable<LandingLinkDTO> {
+    return this.http
+      .get<LandingLinkDTO>(`${this.env.apiBaseUrl}${this.LANDING_PATH}${this.CONFIG_PATH}${this.LINK_DETAIL_PATH}${linkId}`)
       .pipe(catchError((error) => throwError(error.error as ConcenetError)));
   }
 }
--- src/app/modules/app-modules/administration/landing/landing.module.ts
+++ src/app/modules/app-modules/administration/landing/landing.module.ts
@@ -9,9 +9,17 @@ import { SharedModule } from '@shared/shared.module';
 import { FilterDrawerModule } from '@modules/feature-modules/filter-drawer/filter-drawer.module';
 // eslint-disable-next-line max-len
 import { AdministrationCommonHeaderSectionModule } from '@modules/feature-modules/administration-common-header-section/administration-common-header-section.module';
+import { MediaViewerDialogModule } from '@modules/feature-modules/media-viewer-dialog/media-viewer-dialog.module';
 
 @NgModule({
   declarations: [LandingComponent, LandingConfigComponent, LandingLinksComponent, LandingBannersComponent],
-  imports: [CommonModule, LandingRoutingModule, SharedModule, FilterDrawerModule, AdministrationCommonHeaderSectionModule]
+  imports: [
+    CommonModule,
+    LandingRoutingModule,
+    SharedModule,
+    FilterDrawerModule,
+    AdministrationCommonHeaderSectionModule,
+    MediaViewerDialogModule
+  ]
 })
 export class LandingModule {}
