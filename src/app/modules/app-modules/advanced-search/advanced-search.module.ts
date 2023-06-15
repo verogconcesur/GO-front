ef9ddef2bfaea9e7ce58dde518c1f6c6b1be797e
Fernando Jesús GONZÁLEZ MARTÍ <fernandoj.gonzalez@sdos.local>
2023-06-15 14:41:54 +0200

    Features/advanced search

--- src/app/modules/app-modules/advanced-search/advanced-search.module.ts
+++ src/app/modules/app-modules/advanced-search/advanced-search.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { AdvancedSearchRoutingModule } from './advanced-search-routing.module';
 import { AdvancedSearchComponent } from './advanced-search.component';
+import { AdvSearchCardTableComponent } from './components/adv-search-card-table/adv-search-card-table.component';
 
 @NgModule({
-  declarations: [AdvancedSearchComponent],
+  declarations: [AdvancedSearchComponent, AdvSearchCardTableComponent],
   imports: [CommonModule, SharedModule, AdvancedSearchRoutingModule]
 })
 export class AdvancedSearchModule {}
