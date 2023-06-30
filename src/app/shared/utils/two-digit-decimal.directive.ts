e71e2021b1fbc16acd1f5032cc51b969ce78ec3b
Ignacio.castrillon <ignacio.castrillon@alten.es>
2023-06-30 11:56:23 +0200

    feat: decimal digit directive fixed

--- src/app/shared/utils/two-digit-decimal.directive.ts
+++ src/app/shared/utils/two-digit-decimal.directive.ts
@@ -6,26 +6,23 @@ import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 })
 export class TwoDigitDecimalDirective {
   @Input() decimals = 2;
-  // Allow decimal numbers and negative values
-  // private defaultRegex = new RegExp(/^\d*\.?\d{0,2}$/g);
-  // Allow key codes for special events. Reflect :
-  // Backspace, tab, end, home
-  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Del', 'Delete'];
+  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Del', 'Delete', '-'];
 
   constructor(private el: ElementRef) {}
+
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
-    // console.log(this.el.nativeElement.value);
-    // Allow Backspace, tab, end, and home keys
     if (this.specialKeys.indexOf(event.key) !== -1) {
       return;
     }
+
     const current: string = this.el.nativeElement.value;
     const position = this.el.nativeElement.selectionStart;
-    const next: string = [current.slice(0, position), event.key === 'Decimal' ? '.' : event.key, current.slice(position)].join(
-      ''
-    );
-    const regex = new RegExp('^\\d*\\.?\\d{0,' + this.decimals.toString() + '}$', 'g');
+    const next: string =
+      [current.slice(0, position), event.key === 'Decimal' ? '.' : event.key, current.slice(position)].join('');
+
+    const regex = new RegExp('^-?\\d*\\.?\\d{0,' + this.decimals.toString() + '}$', 'g');
+
     if (next && !String(next).match(regex)) {
       event.preventDefault();
     }
