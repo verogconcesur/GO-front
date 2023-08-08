343f024ebf4160aa97d9f4458025a304bfa9949c
Fernando Jesús GONZÁLEZ MARTÍ <fernandoj.gonzalez@sdos.local>
2023-08-08 14:42:10 +0200

    Implementation of calendar view

--- src/app/modules/app-modules/workflow/submodules/workflow-calendar-view/subcomponents/workflow-calendar-hour-line/workflow-calendar-hour-line.component.ts
+++ src/app/modules/app-modules/workflow/submodules/workflow-calendar-view/subcomponents/workflow-calendar-hour-line/workflow-calendar-hour-line.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import WorkflowCalendarHourLineDTO, { WorkflowCalendarDayDTO } from '@data/models/workflows/workflow-calendar-hour-line-dto';
+import _ from 'lodash';
 import moment, { Moment } from 'moment';
 
 @Component({
@@ -7,17 +8,32 @@ import moment, { Moment } from 'moment';
   templateUrl: './workflow-calendar-hour-line.component.html',
   styleUrls: ['./workflow-calendar-hour-line.component.scss']
 })
-export class WorkflowCalendarHourLineComponent implements OnInit {
+export class WorkflowCalendarHourLineComponent implements OnInit, AfterViewInit {
   @Input() mode: 'WEEK' | 'DAY' = 'WEEK';
   @Input() first = false;
   @Input() workflowHourLine: WorkflowCalendarHourLineDTO;
-
+  public isCurrentHour = false;
   constructor() {}
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.isCurrentHour && document.getElementById('WorkflowLine')) {
+        console.log(document.getElementById('WorkflowLine').offsetHeight);
+        const offsetTop = (document.getElementById('WorkflowLine').offsetHeight / 60) * moment().get('minutes');
+        const styleTop = 'top:' + offsetTop + 'px';
+        document.getElementById('WorkflowTimeLine').setAttribute('style', styleTop);
+      }
+    }, 1000);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isCurrentHour =
+      this.workflowHourLine.hour.isSame(moment(), 'hour') &&
+      this.workflowHourLine.days.length &&
+      _.some(this.workflowHourLine.days, (dayLine: WorkflowCalendarDayDTO) => dayLine.day.isSame(moment(), 'day'));
+  }
   public compareTime(day: Moment, hour?: Moment): boolean {
     if (hour) {
-      return day.isSame(moment(), 'day') || hour.isSame(moment(), 'hour');
+      return day.isSame(moment(), 'day') || this.isCurrentHour;
     } else {
       return day.isSame(moment(), 'day');
     }
